fd4d1183bc877d91cb62a60a0266d4946b8357ff
MonikaChakraborty <mchakraborty7516@gmail.com>
2023-08-19 18:02:14 +0600

    parallelogram done using reusable function

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -39,3 +39,32 @@ function calculateRectangleArea(){
     const rectangleAreaSpan = document.getElementById('rectangle-area').innerText = area;
 
 }
+
+// reusable function  -->  reduce duplicate code
+
+function calculateParallelogramArea(){
+    const base = getInputValue('parallelogram-base');
+
+    const height = getInputValue('parallelogram-height'); 
+
+    const area = base * height;
+    setElementInnerText('parallelogram-area', area);
+
+}
+
+
+// reusable get input value field in number
+
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+
+
+// reusable set span, p, div etc text
+function setElementInnerText(elementId, area){
+     const element = document.getElementById(elementId);
+     element.innerText = area;
+}
