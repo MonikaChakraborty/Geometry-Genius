879d61edea97df8125d797a57e9474b512dc94e4
MonikaChakraborty <mchakraborty7516@gmail.com>
2023-08-19 18:20:06 +0600

    ellipse area calculation done

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -52,6 +52,14 @@ function calculateParallelogramArea(){
 
 }
 
+function calculateEllipseArea(){
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const minorRadius = getInputValue('ellipse-minor-radius');
+
+    const area = 3.14 * majorRadius * minorRadius;
+    setElementInnerText('ellipse-area', area);
+}
+
 
 // reusable get input value field in number
 
